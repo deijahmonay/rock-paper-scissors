2a7abfd5eff8e5ca727a95f17192505e6623c36a
deijahmonay <deijah@shack15.com>
2024-08-08 19:50:33 -0700

    set up completed

---
+++ js/app.js
@@ -0,0 +1,30 @@
+/*-------------------------------- Constants --------------------------------*/
+// 1) 3 user choices of rock, paper or scissors
+    //Dom
+
+
+/*-------------------------------- Variables --------------------------------*/
+// 2) Define our variables for state
+    //The players choice
+    // The computers choice
+    //The match result - win/lose/tie
+    //A result message - display the result/ who won
+/*------------------------ Cached Element References ------------------------*/
+// 3) Reference to DOM element to disply messages
+    // 3 button DOM references for rock, paper, scissors
+/*-------------------------------- Functions --------------------------------*/
+//  handle player click
+// handle generating random selections for the computer player
+// compare player & comouter chpices - check for winner
+    //if/else comparison to all choices
+        //rock beats scissors
+        //scissors beats paper
+        // paper beats rock
+        // same option - its a tie
+        
+// render a win, lose ,tie message to the player
+    // including player and computer choices in the message
+    // clearly indiciating who won/result
+
+/*----------------------------- Event Listeners -----------------------------*/
+// Handle a player clicking a button
